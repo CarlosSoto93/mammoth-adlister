421e0051f32587b6bcec0da06307473d5e1803af
Carlos <c.soto93@hotmail.com>
2017-06-12 13:52:47 -0500

    JDBC exercise

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,5 +1,72 @@
-/**
- * Created by Carlos on 6/9/17.
- */
-public class MySQLAdsDao {
+
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MySQLAdsDao implements Ads {
+    private Connection connection = null;
+
+    public MySQLAdsDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to database", e);
+        }
+    }
+
+    @Override
+    public List<Ad> all() {
+        try {
+            Statement stm = connection.createStatement();
+            ResultSet rs = stm.executeQuery("SELECT * FROM ads");
+            return createAdsFromResults(rs);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error retrieving all ad", e);
+        }
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        try {
+            Statement stm = connection.createStatement();
+            stm.executeUpdate(createInsertQuery(ad), Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = stm.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error creating new ad.", e);
+        }
+    }
+
+    private String createInsertQuery(Ad ad) {
+        return "INSERT INTO ads (user_id, title, description) VALUES"
+                + "(" + ad.getUserId() + ", "
+                + "'" + ad.getTitle() + ", "
+                + "'" + ad.getDescription() + "')";
+    }
+
+    private Ad extractAds(ResultSet rs) throws SQLException {
+        return new Ad(
+                rs.getLong("id"),
+                rs.getLong("user_id"),
+                rs.getString("title"),
+                rs.getString("description")
+        );
+    }
+
+    private List<Ad> createAdsFromResults(ResultSet rs) throws SQLException {
+        List<Ad> ads = new ArrayList<>();
+        while (rs.next()) {
+            ads.add(extractAds(rs));
+        }
+        return ads;
+    }
 }
